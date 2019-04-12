b9b636450f94d1b93e5deb7b5f500eb93942ab7b
jorgehere <murillojorge42@gmail.com>
2019-04-11 19:40:32 -0700

    put pager

--- sjsulist/src/ItemCardlistComponent/ItemCardList.js
+++ sjsulist/src/ItemCardlistComponent/ItemCardList.js
@@ -5,6 +5,7 @@ import './ItemCardList.css';
 const ItemCardList = () =>{
     return(
     <div>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <ul class="store-list">
             <li><ItemCard/></li>
@@ -16,6 +17,18 @@ const ItemCardList = () =>{
             <li><ItemCard/></li>
             <li><ItemCard/></li>
         </ul>
+        <div >
+            <ul class="pagination pager">
+                <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+                <li class="active"><a href="#!">1</a></li>
+                <li class="waves-effect"><a href="#!">2</a></li>
+                <li class="waves-effect"><a href="#!">3</a></li>
+                <li class="waves-effect"><a href="#!">4</a></li>
+                <li class="waves-effect"><a href="#!">5</a></li>
+                <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+            </ul>
+        </div>
+        
     </div>
     );
 }
