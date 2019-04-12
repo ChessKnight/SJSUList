7ce39e01469c042f2c8ee90b1c2bdbd126c4957f
siddsaad <saadahmedsiddiqui.01@gmail.com>
2019-04-11 20:01:41 -0700

    item form updated

--- sjsulist/src/ItemCardlistComponent/ItemCardList.js
+++ sjsulist/src/ItemCardlistComponent/ItemCardList.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ItemCard from './ItemCard';
 import './ItemCardList.css';
 
+import ItemForm from '../ItemForm/ItemForm.js';
+
 const ItemCardList = () =>{
     return(
     <div>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <ul class="store-list">
+            
+            <ItemForm></ItemForm>
+
             <li><ItemCard/></li>
             <li><ItemCard/></li>
             <li><ItemCard/></li>
@@ -16,6 +21,8 @@ const ItemCardList = () =>{
             <li><ItemCard/></li>
             <li><ItemCard/></li>
         </ul>
+
+            
     </div>
     );
 }
---
+++ sjsulist/src/ItemForm/ItemForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+
+class ItemForm extends Component {
+
+  render() {
+    return (
+        <div>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+            <form action="#">
+
+                {/* Image Upload */}
+                <div class="file-field input-field">
+
+                    <div class="btn">
+                        <span>File</span>
+                        <input type="file"></input>
+                    </div>
+
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text"></input>
+                    </div>
+
+                </div>
+
+                {/* Name of item */}
+                <div class="row">
+                    <div class="input-field col s6">
+                    <i class="material-icons prefix"> description</i>
+                        <input placeholder="Item" id="first_name" type="text" class="validate"></input>
+                        <label for="Name of Item">Name Of Item</label>
+                    </div>
+                </div>
+            
+            {/* Item Description */}
+                <div class="row">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix"> description</i>
+                        <textarea id="textarea2" class="materialize-textarea" data-length="120"></textarea>
+                        <label for="textarea2">Item description</label>
+                    </div>
+                </div>
+
+                {/* Contact info */}
+                <div class="row">
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input id="icon_prefix" type="text" class="validate"></input>
+                        <label for="icon_prefix">Your Name</label>
+                    </div>
+        
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">phone</i>
+                        <input id="icon_telephone" type="tel" class="validate"></input>
+                        <label for="icon_telephone">Contact#</label>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    )
+  }
+}
+
+export default ItemForm;
