06647e9a335d778c5e1721ef6e0a632abb19f5b1
tahsin <tahsin.hossain@sjsu.edu>
2019-04-22 21:23:00 -0700

    database connected , model, controller, router added

--- sjsulist/server.js
+++ sjsulist/server.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const server = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config();   
+
+
+//connecting with database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {console.log('database connected')});
+mongoose.connection.on("error", err => {
+  console.log(`error message: ${err.message}`)});
+
 
 server.get('/', (req, res)=>{
   res.send('test from new server')
