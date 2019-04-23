cc30d3463e3ee8c2a5d807e2f0d3e5c5a12ddecc
tahsin <tahsin.hossain@sjsu.edu>
2019-04-22 22:12:43 -0700

    fixed error

--- sjsulist/controller/ItemController.js
+++ sjsulist/controller/ItemController.js
@@ -2,18 +2,19 @@ const Item = require('../model/ItemModel')
 
 
 exports.getItem = (_req, res) => {
-  Item.find((err, Item ) => {
+  const item = Item.find()
+  item.find((err, item ) => {
       if (err) res.select("_id body").status(400).send(err);
-    res.json(Item ).status(200); 
+    res.json(item ).status(200); 
   })
 }
 
 
 exports.addItem = (req, res) => {
-    const Item = new Item(req.body) 
-  Item .save((err) => {
+    const item = new Item(req.body) 
+  item.save((err) => {
        if (err) res.status(500).send(err);
-    res.json(Item ).status(200);
+    res.json(item).status(200);
      });
 };
 
