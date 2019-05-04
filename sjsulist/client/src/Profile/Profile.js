e3b5ece8aec4bd6f03acfa1715d584facf40bc2a
tahsin <tahsin.hossain@sjsu.edu>
2019-05-04 11:04:47 -0700

    getting data from backend (profile)

--- sjsulist/client/src/Profile/Profile.js
+++ sjsulist/client/src/Profile/Profile.js
@@ -4,26 +4,45 @@ import './Profile.css';
 import User from './User';
 import GroupList from './GroupList';
 import Store from './Store';
+import axios from 'axios'
+import UserInfo from "./UserInfo";
 
 class Profile extends Component{
-    state={
-        profileImage:""
+    constructor() {
+        super()
+        this.state = {
+            profileImage:"",
+            name: "", 
+            email: "",
+            studentMajor: "",
+            user: [],
+            created: ""
     }
+}
 
+     
     componentDidMount(){
         //this is where it fetches the data based on the user id
+        const userId = this.props.match.params.userId;
+        axios.get(`http://localhost:5000/userby/${userId}`)
+            .then(res => console.log(res.data)) 
     }
 
     render(){
+        console.log(this.state.user.name)
         return(
             <div className="profile">
-                <User></User>
+            
+                <User>
+                    <UserInfo name={this.state.user.name}/> 
+                </User>
                 <Container value="Groups">
                     <GroupList></GroupList>
                 </Container>
                 <Container value="Store">
                     <Store></Store>
                 </Container>
+                 
             </div>
         );
     }
